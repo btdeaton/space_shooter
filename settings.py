9ce6885a4c28daebb64c4383fb57f4dbd3d66a68
btdeaton <blainetdeaton@gmail.com>
2024-05-08 09:17:23 -0400

    Added Play button, levels, and scores

--- settings.py
+++ settings.py
@@ -2,10 +2,9 @@ class Settings:
     """A class to store all settings for Space Shooter"""
 
     def __init__(self):
-        """Initialize the settings for Space Shooter"""
+        """Initialize the static settings for Space Shooter"""
 
         #Ship settings
-        self.ship_speed = 1.5
         self.ship_limit = 3
         
         #Screen settings for width, height, and color
@@ -14,14 +13,37 @@ class Settings:
         self.background_color = (230, 230, 230)
 
         #Bullet settings
-        self.bullet_speed = 2.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
         #Alien settings
-        self.alien_speed = 1.0
         self.fleet_drop_speed = 10
+
+        #How quickly the game speeds up
+        self.speedup_scale = 1.1
+        #How quickly the alien point values increase
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize the game's dynamic settings that change throughout the game"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.5
+        self.alien_speed = 1.0
+
         #fleet_direction where 1 represents right, -1 represents left
         self.fleet_direction = 1
+
+        #Score settings
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """Increase the speed settings and alien point values"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
