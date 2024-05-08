0d98ae98b3c86cc5a1222569f8dad01bfd9465d2
btdeaton <blainetdeaton@gmail.com>
2024-05-08 09:17:23 -0400

    Added Play button, levels, and scores

--- space_shooter.py
+++ space_shooter.py
@@ -3,6 +3,8 @@ from time import sleep
 import pygame
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoredboard
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -22,8 +24,9 @@ class SpaceShooter:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Space Shooter")
 
-        #Create instance to store game statistics
+        #Create instance to store game statistics and create scoreboard
         self.stats = GameStats(self)
+        self.sb = Scoredboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -31,8 +34,11 @@ class SpaceShooter:
 
         self._create_fleet()
 
-        #Start Space Shooter in an active state
-        self.game_active = True
+        #Start Space Shooter in an inactive state
+        self.game_active = False
+
+        #Make the Play button
+        self.play_button = Button(self, "Play")
 
     def run_game(self):
         """Create the main loop to run the game"""
@@ -57,6 +63,31 @@ class SpaceShooter:
                     self._check_keydown_events(event)
                 elif event.type == pygame.KEYUP:
                     self._check_keyup_events(event)
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = pygame.mouse.get_pos()
+                    self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the player presses play"""
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.game_active:
+            #Reset the game settings
+            self.settings.initialize_dynamic_settings()
+            #Hide the mouse cursor
+            pygame.mouse.set_visible(False)
+
+            #Reset the game stats
+            self.stats.reset_stats()
+            self.sb.prep_score()
+            self.game_active = True
+
+            #Get rid of any remaining bullets and aliens
+            self.bullets.empty()
+            self.aliens.empty()
+
+            #Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
                     
     def _check_keydown_events(self, event):
         """If key is pressed down, update the flags"""
@@ -69,6 +100,9 @@ class SpaceShooter:
         #If Q is pressed quit the game
         elif event.key == pygame.K_q:
             sys.exit()
+        #If P is pressed start the game
+        elif event.key == pygame.K_p:
+            self.game_active = True
         #If spacebar is pressed fire bullet
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
@@ -105,10 +139,16 @@ class SpaceShooter:
         #Check if any bullets have hit aliens, if so get rid of both
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+
         #If fleet is destroyed, destroy all bullets and create new fleet
         if not self.aliens:
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _ship_hit(self):
         """Respond to ship being hit by alien"""
@@ -128,6 +168,7 @@ class SpaceShooter:
             sleep(0.5)
         else:
             self.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _create_fleet(self):
         """Create the fleet of aliens"""
@@ -193,6 +234,13 @@ class SpaceShooter:
             bullet.draw_bullet() 
         self.ship.blitme()
         self.aliens.draw(self.screen)
+
+        #Draw the score
+        self.sb.show_score()
+
+        #Draw the play button if the game is inactive
+        if not self.game_active:
+            self.play_button.draw_button()
 
         #Make most recent screen visible
         pygame.display.flip()
